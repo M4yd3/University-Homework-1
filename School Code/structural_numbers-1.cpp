42e6e4ab84a2b8adee4f979bc6a15a2f2fde4a79
m4yd3 <marchenko151003@mail.ru>
2021-09-06 08:53:39 +0300

    Add structural_numbers-1

---
+++ School Code/structural_numbers-1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string> 
+#include <set>
+
+using namespace std;
+
+int main() {
+	int m, n;
+	cin >> m >> n;
+	if (!cin) {
+		cout << "Wrong input";
+		return -1;
+	}
+	set<string> ans;
+	for (int i = m; i < n; i++) {
+		for (int k = i + 1; k <= n; k++) {
+			int x = k * k - i * i;
+			int y = 2 * k * i;
+			int z = k * k + i * i;
+			
+			int t = min(x, y);
+			y = max(x, y);
+			x = t;
+
+			int xs = x, ys = y, zs = z;
+
+			int j = 1;
+			while (z * j <= n) {
+				string str = to_string(x * j) + " " + to_string(y * j) + " " + to_string(z * j);
+				ans.insert(str);
+				cout << j << " " << z * j << " " << n << endl;
+				j++;
+			}
+		}
+	}
+	for (string row : ans) {
+		cout << row << endl;
+	}
+}
