f36930a0782cb7d4fb48322914687a7a75d2461f
m4yd3 <marchenko151003@mail.ru>
2021-09-21 22:03:22 +0300

    Update 3-4.cpp

--- School Code/3-4.cpp
+++ School Code/3-4.cpp
@@ -6,21 +6,25 @@ using namespace std;
 
 int main() {
     ofstream file_write("text.txt");
-    string input; 
-    getline(cin, input);
+    string input = "1 12d d12 35 1-1  -21 -2 20.1 -03.13 .13 -.31 ..1 1.1";
+    //getline(cin, input);
     file_write << input;
     file_write.close();
 
     ifstream file_read("text.txt");
     string line;
     getline(file_read, line);        
+    cout << line << endl;
     file_read.close();
 
     smatch m;
-    const regex e(R"((?:(?<=\s)|^)(-?(?:\d+(?:\.\d+)?|\.\d+))(?=$|\s))");
-
-    while (regex_search(line, m, e)) {
-        cout << m[1] << endl;
-        line = m.suffix().str();
+    try {
+        const regex e(R"((?:\s|^)(-?(?:\d+(?:\.\d+)?|\.\d+))(?=\s|,|$))");
+        while (regex_search(line, m, e)) {
+            cout << m[1] << endl;
+            line = m.suffix().str();
+        }
+    } catch (exception e) {
+        cout << "Error";
     }
 }
