83e1f630361f1c09bae19a1e85dea7aaf6235236
M4yd3 <marchenko151003@mail.ru>
2021-10-15 15:07:16 +0300

    Add files-2

--- School Code/files-1.cpp
+++ School Code/files-1.cpp
@@ -5,51 +5,87 @@
 #include <regex>
 using namespace std;
 
-struct Toy {
-    string name;
-    double cost;
+struct Book {
+    string title;
+    int year;
     vector<int> age;
 
-    Toy(string name, double cost, vector<int> age) {
-        this->name = name;
-        this->cost = cost;
+    Book(string name, double cost, vector<int> age) {
+        this->title = name;
+        this->year = cost;
         this->age = age;
     }
 
     string convert_to_string() {
-        return name + ";" + to_string(cost) + ";" + to_string(age[0]) + "," + to_string(age[1]);
+        return title + ";" + to_string(year) + ";" + to_string(age[0]) + "," + to_string(age[1]);
+    }
+
+    bool fits(int value) {
+        return value >= age[0] && value <= age[1];
     }
 };
 
-static Toy parse_from_string(string value) {
+static Book parse_from_string(string value) {
     vector<string> parts;
     while (value.find(";") != -1) {
         parts.push_back(value.substr(0, value.find(";")));
         value = value.substr(value.find(";") + 1, value.size());
     }
     if (value.size() != 0) parts.push_back(value);
-    
-    if (parts.size() > 3) {
-        int size = parts.size();
-        string name;
-        for (int i = 0; i < size - 2; i++) {
-            name += parts[i];
-        }
-        parts.at(0) = name;
-        parts.at(1) = parts[size - 2];
-        parts.at(2) = parts[size - 1];
-        parts.resize(3);
-    }
 
     int comma = parts[2].find(",");
     vector<int> age = {stoi(parts[2].substr(0, comma)), stoi(parts[2].substr(comma + 1, parts[2].size() - 1)) };
 
-    return Toy(parts[0], stoi(parts[1]), age);
+    return Book(parts[0], stoi(parts[1]), age);
 }
 
-int main() {
-    //ofstream file_write("text.json");
-    Toy toy = Toy("Soldier", 400, {3, 7});
-    cout << parse_from_string("Sol;di;er;400;3,7").convert_to_string();
+const vector<string> names = {"Toy Soldier", "Toy Doll", "Toy Car", "Lego Set", "Building Blocks", "Ball", "Tetris", "NERF Gun"};
 
+int main() {
+    cout << "Input amount of toys to generate: ";
+    int n; cin >> n;
+    if (!cin || n == 0) {
+        cout << "Invalid input";
+        return -1;
+    }
+    cout << "Generating toys and writing them into file..." << endl;
+
+    srand((unsigned)time(NULL));
+    ofstream file_write("text.txt");
+    for (int i = 0; i < n; i++) {
+        int a1 = 3 + rand() % 15;
+        int a2 = a1 + 1 + rand() % (18 - a1);
+        Book toy = Book(names[rand() % names.size()] + " " + to_string(i), 
+            50 + rand() % 4951, {a1, a2});
+        file_write << toy.convert_to_string() << endl;
+    }
+    file_write.close();
+    cout << "File created successfully" << endl << endl;
+
+    ifstream file_read("text.txt");
+    string line;
+
+    cout << "Answer A:" << endl;
+    while (getline(file_read, line)) {
+        Book toy = parse_from_string(line);
+        if (toy.year <= 400 && toy.fits(8)) cout << toy.title << endl;
+    }
+    file_read.close();
+
+    file_read.open("text.txt");
+    int m = -1;
+    while (getline(file_read, line)) {
+        Book toy = parse_from_string(line);
+        m = max(m, toy.year);
+    }
+    cout << endl << "Answer B: " << m << endl << endl;
+    file_read.close();
+
+    file_read.open("text.txt");
+    cout << "Answer C:" << endl;
+    while (getline(file_read, line)) {
+        Book toy = parse_from_string(line);
+        if (toy.fits(4) && toy.fits(10)) cout << toy.title << ", " << toy.year << endl;
+    }
+    file_read.close();
 }
---
+++ School Code/files-2.cpp
@@ -0,0 +1,92 @@
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cctype>
+#include <regex>
+using namespace std;
+
+struct Book {
+    string author;
+    string title;
+    int year;
+
+    Book(string author, string title, double year) {
+        this->author = author;
+        this->title = title;
+        this->year = year;
+    }
+
+    string convert_to_string() {
+        return author + ";" + title + ";" + to_string(year);
+    }
+};
+
+static Book parse_from_string(string value) {
+    vector<string> parts;
+    while (value.find(";") != -1) {
+        parts.push_back(value.substr(0, value.find(";")));
+        value = value.substr(value.find(";") + 1, value.size());
+    }
+    if (value.size() != 0) parts.push_back(value);
+    return Book(parts[0], parts[1], stoi(parts[2]));
+}
+
+static string string_to_lower(string data) {
+    string new_value;
+    for (char i : data) {
+        new_value += tolower(i);
+    }
+    return new_value;
+}
+
+int main() {
+    const regex r(R"(^\w+\;\w+\;\d+$)");
+    ofstream file_write("text.txt");
+    string input;
+
+    cout << "Input books like this: \"Author\";\"Title\";\"Year\"" << endl;
+
+    int i(0);
+    while (true) {
+        cout << "Inputting book #" << i + 1 << endl;
+        getline(cin, input);
+        if (!regex_match(input, r)) {
+            break;
+        }
+        Book book = parse_from_string(input);
+        file_write << book.convert_to_string() << endl;
+        i++;
+    }
+    file_write.close();
+    cout << "Input cancelled" << endl << endl;
+
+    ifstream file_read("text.txt");
+    string line;
+
+    cout << "Input author:" << endl;
+    string author; cin >> author;
+    cout << "Input year:" << endl;
+    int year; cin >> year;
+    if (!cin) {
+        cout << "Invalid input";
+        return -1;
+    }
+    while (getline(file_read, line)) {
+        Book book = parse_from_string(line);
+        if (string_to_lower(book.author) == string_to_lower(author) && book.year == year) 
+            cout << "Your book is \"" << book.title << "\"" << endl;
+    }
+    file_read.close();
+
+    file_read.open("text.txt");
+    string s = "No such book";
+    while (getline(file_read, line)) {
+        Book book = parse_from_string(line);
+        if (string_to_lower(book.title).find("pascal") != -1)
+            s = "Book's author and year are: " + book.author + ", " + to_string(book.year);
+    }
+    cout << endl << s << endl << endl;
+    file_read.close();
+}
