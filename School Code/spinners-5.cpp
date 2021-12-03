9e2ae6086002518e6bce1627e811648a84e75fbf
m4yd3 <marchenco151003@mail.ru>
2021-12-03 14:02:52 +0300

    Add spinners-5

--- School Code/spinners-5.cpp
+++ School Code/spinners-5.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
+	long n, k; cin >> n >> k;
+	vector<long> seats{ n };
 
+	long l, r;
+	for (long i = 0, m; i < k; i++) {
+		m = 0;
+		for (long j = 1; j < size(seats); j++) if (seats[m] < seats[j]) m = j;
+		r = seats[m] / 2;
+		l = seats[m] - 1 - seats[m] / 2;
+		seats.erase(seats.begin() + m);
+		if (r != 0) seats.insert(seats.begin() + m, r);
+		if (l != 0) seats.insert(seats.begin() + m, l);
+	}
+
+	cout << l << endl << r;
 }
