c0bdcf7976dff31d4d8c087ff775bb4431b88a77
m4yd3 <marchenko151003@mail.ru>
2021-12-17 15:14:44 +0300

    Оформление 100 задачи

--- School Code/additional-100.cpp
+++ School Code/additional-100.cpp
@@ -5,20 +5,25 @@
 #include <array>
 using namespace std;
 
-// 97 - 122, %26
-char rand_char() { return (97 + rand() % 5); }
+char rand_char(int n) { return ('a' + rand() % n); }
 
 int cell_int(char cell) { return (int)cell - 48; }
 
+char cell_out(char cell) { return "0123456789AB"[cell_int(cell)]; }
+
 int main() {
 	srand((unsigned)time(NULL));
+
+	cout << "Enter number of letters [1-26]: ";
+	int n;
+	cin >> n;
+	if (!cin || n < 1 || n > 26) return -1;
 	
-	// Fill work.dat with initial random data.
 	ofstream work_dat("work.dat");
 	fstream work_out("work.out");
 	for (int i = 0; i < 21; i++) {
 		for (int j = 0; j < 21; j++) {
-			char ch = rand_char();
+			char ch = rand_char(n);
 			work_dat << ch << " ";
 		}
 		work_dat << endl;
@@ -29,65 +34,61 @@ int main() {
 	array<array<int, 21>, 21> future;
 
 	cout << "Enter number of generations: ";
-	int steps; cin >> steps;
+	int steps;
+	cin >> steps;
 	if (!cin) return 1;
 
-	char symbol = rand_char();
+	char symbol = rand_char(n);
 	ifstream fin("work.dat");
 	cout << endl << "Generation 0" << endl;
 	int c = 0;
 	for (int i = 0; i < 21; i++) {
 		char ch;
 		for (int j = 0; j < 21 && fin >> ch; j++) {
-			char cell = (ch == symbol ? '1' : 'x');
+			char cell = (ch == symbol ? '1' : 'X');
+			present[i][j] = cell;
 			work_out << cell << " ";
 			cout << cell << " ";
-			present[i][j] = cell;
-			c += cell != 'x';
+			c += cell != 'X';
 		}
 		cout << endl;
 		work_out << endl;
 	}
 	fin.close();
-
 	cout << "Living cells: " << c << endl;
 
 	for (int i = 0; i < steps && c != 0; i++) {
 		cout << endl << "Generation " << i + 1;
 		c = 0;
 		cout << endl;
-		work_out.clear();
+		work_out << endl;
 		for (int i = 0; i < 21; i++) {
 			for (int j = 0; j < 21; j++) {
 				char cell = present[i][j];
-				if (cell == 'x') {
-					cout << cell;
+				if (cell == 'X') {
+					work_out << "X ";
+					cout << "X ";
 				}
 				else {
-					if (cell_int(cell) > 0 && cell_int(cell) < 11) {
+					if (cell_int(cell) < 11) {
 						int n = 0;
 						for (int k = max(i - 1, 0); k <= min(i + 1, 20); k++) {
 							for (int l = max(j - 1, 0); l <= min(j + 1, 20); l++) {
 								int t = cell_int(present[k][l]);
-								if (!(k == i && l == j) && present[k][l] != 'x' && t >= 1 && t <= 11) n++;
+								if (!(k == i && l == j) && present[k][l] != 'X' && t >= 1) n++;
 							}
 						}
-						if (n == 2 || n == 3) {
+						if ((cell_int(cell) > 0 && n == 2 || n == 3) || (cell == 0 && n == 3)) {
 							cell++;
 							c++;
 						}
 						else cell = '0';
 					}
-					else if (cell_int(cell) == 0) {
-						cell++;
-						c++;
-					}
-					else if (cell_int(cell) == 11) cell = '0';
-					cout << cell_int(cell);
+					else cell = '0';
+					work_out << cell_out(cell) << " ";
+					cout << cell_out(cell) << " ";
 				}
-				cout << " ";
 				future[i][j] = cell;
-				work_out << cell << " ";
 			}
 			cout << endl;
 			work_out << endl;
